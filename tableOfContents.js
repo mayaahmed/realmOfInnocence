00bffb9639f183847018cfc648e27dca7b932e84
Maya Mohsin Ahmed <maya.ahmed@gmail.com>
2017-02-09 13:14:55 +0530

    Add files via upload

--- tableOfContents.js
+++ tableOfContents.js
@@ -2,17 +2,18 @@
  var tableDiv = document.getElementById("tableOfContentsDiv");
 Home = "<a href='index.html' class='tocLinks'> Home </a>";
 
-chapter1 = " <br/> <br/> <a href='1eechamotta.html' class='tocLinks'>1. The fear of Eecha Motta  </a>";
+chapter1 = " <br/> <br/> <a href='1eechamotta.html' class='tocLinks'>1. The Fear Of Eecha Motta  </a>";
 chapter2 = " <br/> <br/> <a href='2stolentoy.html' class='tocLinks'>2. The Stolen Toy </a>";
-chapter3 = " <br/> <br/> <a href='3tamarind.html' class='tocLinks'>3. The Tamarind tale  </a>";
-chapter4= " <br/> <br/> <a href='4meangirls.html' class='tocLinks'>4. Mean girls </a>";
-chapter5 = " <br/> <br/> <a href='5dress.html' class='tocLinks'> 5. The Dress disappointment </a>";
-
-
-
-
+chapter3 = " <br/> <br/> <a href='3tamarind.html' class='tocLinks'>3. The Tamarind Tale  </a>";
+chapter4= " <br/> <br/> <a href='4meangirls.html' class='tocLinks'>4. Mean Girls </a>";
+chapter5 = " <br/> <br/> <a href='5dress.html' class='tocLinks'> 5. The Dress Disappointment </a>";
+chapter6 = " <br/> <br/> <a href='6toybreakers.html' class='tocLinks'> 6. The Toy Smashers </a>";
+chapter7 = " <br/> <br/> <a href='7dogbite.html' class='tocLinks'> 7. The Dog Bite </a>";
+chapter8 = " <br/> <br/> <a href='8liars.html' class='tocLinks'> 8. The Fablers </a>";
+chapter9 = " <br/> <br/> <a href='9lost.html' class='tocLinks'> 9. Lost </a>";
+chapter10 = " <br/> <br/> <a href='10kickedout.html' class='tocLinks'> 10. Kicked Out Of Home </a>";
 
 function toc(){
-  tableDiv.innerHTML=Home+chapter1+chapter2+chapter3+chapter4+chapter5 ;}
+  tableDiv.innerHTML=Home+chapter1+chapter2+chapter3+chapter4+chapter5+chapter6+chapter7+chapter8+chapter9+chapter10 ;}
 
 toc();
